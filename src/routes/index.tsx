3c516e07d2d130934a0fa2cb84d162556c277d69
Sam Potter <sam.potter@hotmail.com>
2026-04-29 12:16:00 +1000

    Add suspense boundary

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from 'react'
 import { useQuery } from '@tanstack/react-query'
 import { createFileRoute } from '@tanstack/react-router'
 import { createFromFetch } from '@tanstack/react-start/rsc'
@@ -5,18 +6,28 @@ import { createFromFetch } from '@tanstack/react-start/rsc'
 export const Route = createFileRoute('/')({ component: Home })
 
 function Home() {
+  return (
+    <div className="p-8">
+      <h1 className="text-4xl font-bold">Welcome to TanStack Start</h1>
+
+      <p className="mt-4 text-lg">
+        Edit <code>src/routes/index.tsx</code> to get started.
+      </p>
+
+      {/* <Suspense> */}
+        <Child />
+      {/* </Suspense> */}
+    </div>
+  )
+}
+
+function Child() {
   const query = useQuery({
     queryKey: ["ServerComponent"],
     queryFn: () => createFromFetch(fetch('/rsc/ServerComponent'))
   })
-  
-  return (
-    <div className="p-8">
-      <h1 className="text-4xl font-bold">Welcome to TanStack Start</h1>
-      <p className="mt-4 text-lg">
-        Edit <code>src/routes/index.tsx</code> to get started.
-      </p>
-      {query.status === 'success' ? query.data : undefined}
-    </div>
-  )
+
+  if (query.status === 'success') return query.data;
+
+  return undefined;
 }
